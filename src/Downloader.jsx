46f093608bc03b689cd2524dccede7ddbe51780b
utpalendubarman <47846069+utpalendubarman@users.noreply.github.com>
2021-01-02 23:14:40 +0530

    Add files via upload

---
+++ src/Downloader.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Nav from './Nav'
+import './style.css'
+class Downloader extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={url:'',type:''};
+    }
+    urlUpdater=(event)=>{
+        this.setState({url:event.target.value});
+    }
+    typeUpdater=(event)=>{
+        this.setState({type:event.target.value});
+    }
+    submitForm=(event)=>{
+        event.preventDefault();
+        //Fetch data here
+        
+    }
+    render(){
+        return(
+            <div className="wrapper">
+                <Nav/>
+                <div id='data'></div>
+                <div className="container">
+                    <div className="row">
+                        <form onSubmit={this.submitForm}>
+                            <div className="col-sm-8 col-xs-12 col-md-12">
+                                <div className="form-group">
+                                    <label>URL</label>
+                                    <input type='text' onChange={this.urlUpdater} className='form-control'/>
+                                </div>
+                                <div className="form-group">
+                                    <label>Type</label>
+                                    <select onChange={this.typeUpdater} className='form-control'>
+                                        <option value="">--Select Download Type--</option>
+                                        <option value="mp3">MP3</option>
+                                        <option value="mp4">MP4</option>
+                                    </select>
+                                </div>
+                                <div className="form-group">
+                                    <button className='btn btn-success'>Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default Downloader;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Downloader from './Downloader'
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <Downloader/>,
+  document.getElementById('root')
+);
+reportWebVitals();
