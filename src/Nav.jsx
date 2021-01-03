777653c37ee14961292ec29edea4d2ebd671aa91
MeerHamza1421 <hamzababar37@gmail.com>
2021-01-04 00:08:03 +0500

    Added Logo

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
+import Logo from './Assets/Logo.png';
 import 'jquery/dist/jquery'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
+import './Nav.css';
 
-class Nav extends React.Component{
-    render(){
-        return(
+class Nav extends React.Component {
+    render() {
+        return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">Youtube Downloader</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className="nav-item nav-link active" href="#">Downloader <span class="sr-only">(current)</span></a>
+                <a className="navbar-brand" href="#">
+                    <img src={Logo} className="Navbar-Logo" alt="Logo" />
+                </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-item nav-link active" href="#">Downloader <span class="sr-only">(current)</span></a>
+                    </div>
                 </div>
-            </div>
             </nav>
         );
     }
