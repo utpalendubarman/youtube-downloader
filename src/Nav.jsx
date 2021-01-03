562565b6abdf08047d3b33978741b882e3be4e9b
MeerHamza1421 <hamzababar37@gmail.com>
2021-01-04 01:17:59 +0500

    Add menus in the navigation section

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,20 +4,22 @@ import 'jquery/dist/jquery'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
 import './Nav.css';
+import { Link } from 'react-router-dom';
 
 class Nav extends React.Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="#">
+                <Link to="/" className="navbar-brand" >
                     <img src={Logo} className="Navbar-Logo" alt="Logo" />
-                </a>
+                </Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <a className="nav-item nav-link active" href="#">Downloader <span class="sr-only">(current)</span></a>
+                        <Link to="/About" className="nav-item nav-link active" >About </Link>
+                        <Link to="/Contributors" className="nav-item nav-link active" >Contributors </Link>
                     </div>
                 </div>
             </nav>
