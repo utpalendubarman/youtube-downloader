2cd6ceaf1b08f7163064ccf3efe517bfd04d18fa
Anushruti <65386044+anushruti11@users.noreply.github.com>
2021-01-21 13:00:51 +0530

    menu

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,19 +2,28 @@ import React from 'react'
 import 'jquery/dist/jquery'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
+import './style.css'
 
 class Nav extends React.Component{
     render(){
         return(
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-lg">
             <a className="navbar-brand" href="#">Youtube Downloader</a>
+      
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+           
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className="nav-item nav-link active" href="#">Downloader <span class="sr-only">(current)</span></a>
+                <div className="navbar-nav ml-auto">
+                <nav class="nav nav-pills flex-column flex-sm-row">
+                    <a class="flex-sm-fill text-sm-center nav-link active" href="#">Downloader</a>
+                    <a class="flex-sm-fill text-sm-center nav-link" href="#">About</a>
+                    <a class="flex-sm-fill text-sm-center nav-link" href="#">Contributors</a>
+                    
+            </nav>
                 </div>
+                
             </div>
             </nav>
         );
